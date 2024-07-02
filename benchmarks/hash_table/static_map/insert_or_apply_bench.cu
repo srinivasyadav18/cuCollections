273a82e554e3a6d5af6fbf7db04e21d0a98e088c
Srinivas Yadav Singanaboina <ssinganaboin@nvidia.com>
2024-07-02 22:30:11 +0000

    add sync bench

--- benchmarks/hash_table/static_map/insert_or_apply_bench.cu
+++ benchmarks/hash_table/static_map/insert_or_apply_bench.cu
@@ -24,6 +24,9 @@
 
 #include <thrust/device_vector.h>
 #include <thrust/transform.h>
+#include <thrust/reduce.h>
+#include <thrust/sort.h>
+
 
 using namespace cuco::benchmark;
 using namespace cuco::utility;
@@ -40,6 +43,12 @@ std::enable_if_t<(sizeof(Key) == sizeof(Value)), void> static_map_insert_or_appl
   auto const num_keys     = state.get_int64_or_default("NumInputs", defaults::N);
   auto const occupancy    = state.get_float64_or_default("Occupancy", defaults::OCCUPANCY);
   auto const multiplicity = state.get_int64_or_default("Multiplicity", defaults::MULTIPLICITY);
+
+  if (num_keys < multiplicity)
+  {
+    state.skip("num_keys < multiplicty");
+    return;
+  }
 
   std::size_t const size = cuco::detail::int_div_ceil(num_keys, multiplicity) / occupancy;
 
@@ -81,7 +90,8 @@ NVBENCH_BENCH_TYPES(static_map_insert_or_apply,
   .set_name("static_map_insert_or_apply_uniform_multiplicity")
   .set_type_axes_names({"Key", "Value", "Distribution"})
   .set_max_noise(defaults::MAX_NOISE)
-  .add_int64_axis("Multiplicity", defaults::MULTIPLICITY_RANGE);
+  .add_int64_axis("Multiplicity", {1, 10, 100, 1000, 10000, 100000})
+  .add_int64_axis("NumInputs", {1, 10, 100, 1000, 10000, 100000});
 
 NVBENCH_BENCH_TYPES(static_map_insert_or_apply,
                     NVBENCH_TYPE_AXES(defaults::KEY_TYPE_RANGE,
@@ -91,3 +101,119 @@ NVBENCH_BENCH_TYPES(static_map_insert_or_apply,
   .set_type_axes_names({"Key", "Value", "Distribution"})
   .set_max_noise(defaults::MAX_NOISE)
   .add_float64_axis("Occupancy", defaults::OCCUPANCY_RANGE);
+
+template <typename Key, typename Value, typename Dist>
+std::enable_if_t<(sizeof(Key) == sizeof(Value)), void> thrust_insert_or_apply(
+  nvbench::state& state, nvbench::type_list<Key, Value, Dist>)
+{
+  using pair_type = cuco::pair<Key, Value>;
+
+  auto const num_keys     = state.get_int64_or_default("NumInputs", defaults::N);
+  auto const multiplicity = state.get_int64_or_default("Multiplicity", defaults::MULTIPLICITY);
+
+  if (num_keys < multiplicity)
+  {
+    state.skip("num_keys < multiplicty");
+    return;
+  }
+
+  thrust::device_vector<Key> keys(num_keys);
+
+  key_generator gen;
+  gen.generate(dist_from_state<Dist>(state), keys.begin(), keys.end());
+
+  thrust::device_vector<Value> values = keys;
+
+  thrust::device_vector<Key> output_keys(num_keys);
+  thrust::device_vector<Value> output_values(num_keys);
+
+  state.add_element_count(num_keys);
+
+  state.exec(nvbench::exec_tag::sync | nvbench::exec_tag::timer, [&](nvbench::launch& launch, auto& timer) {
+    timer.start();
+        thrust::sort_by_key(thrust::cuda::par.on(launch.get_stream()), keys.begin(), keys.end(), values.begin(), thrust::greater<int>());
+        thrust::reduce_by_key(thrust::cuda::par.on(launch.get_stream()), keys.begin(), keys.end(), values.begin(), output_keys.begin(), output_values.begin());
+    timer.stop();
+ });
+}
+
+template <typename Key, typename Value, typename Dist>
+std::enable_if_t<(sizeof(Key) != sizeof(Value)), void> thrust_insert_or_apply(
+  nvbench::state& state, nvbench::type_list<Key, Value, Dist>)
+{
+  state.skip("Key should be the same type as Value.");
+}
+
+NVBENCH_BENCH_TYPES(thrust_insert_or_apply,
+                    NVBENCH_TYPE_AXES(defaults::KEY_TYPE_RANGE,
+                                      defaults::VALUE_TYPE_RANGE,
+                                      nvbench::type_list<distribution::uniform>))
+  .set_name("thrust_insert_or_apply_uniform_multiplicity")
+  .set_type_axes_names({"Key", "Value", "Distribution"})
+  .set_max_noise(defaults::MAX_NOISE)
+  .add_int64_axis("Multiplicity", {1, 10, 100, 1000, 10000, 100000})
+  .add_int64_axis("NumInputs", {1, 10, 100, 1000, 10000, 100000});
+
+template <typename Key, typename Value, typename Dist>
+std::enable_if_t<(sizeof(Key) == sizeof(Value)), void> static_map_insert_or_apply_sync(
+  nvbench::state& state, nvbench::type_list<Key, Value, Dist>)
+{
+  using pair_type = cuco::pair<Key, Value>;
+
+  auto const num_keys     = state.get_int64_or_default("NumInputs", defaults::N);
+  auto const occupancy    = state.get_float64_or_default("Occupancy", defaults::OCCUPANCY);
+  auto const multiplicity = state.get_int64_or_default("Multiplicity", defaults::MULTIPLICITY);
+
+  if (num_keys < multiplicity)
+  {
+    state.skip("num_keys < multiplicty");
+    return;
+  }
+
+  std::size_t const size = cuco::detail::int_div_ceil(num_keys, multiplicity) / occupancy;
+
+  thrust::device_vector<Key> keys(num_keys);
+
+  key_generator gen;
+  gen.generate(dist_from_state<Dist>(state), keys.begin(), keys.end());
+
+  thrust::device_vector<pair_type> pairs(num_keys);
+  thrust::transform(keys.begin(), keys.end(), pairs.begin(), [] __device__(Key const& key) {
+    return pair_type(key, static_cast<Value>(key));
+  });
+
+  thrust::device_vector<Key> output_keys(num_keys);
+  thrust::device_vector<Value> output_values(num_keys);
+
+  state.add_element_count(num_keys);
+
+  state.exec(nvbench::exec_tag::sync | nvbench::exec_tag::timer, [&](nvbench::launch& launch, auto& timer) {
+
+    timer.start();
+
+    cuco::static_map map{size, cuco::empty_key<Key>{-1}, cuco::empty_value<Value>{0}};
+    map.insert_or_apply(
+      pairs.begin(), pairs.end(), cuco::op::reduce::sum, {launch.get_stream()});
+    map.retrieve_all(output_keys.begin(), output_values.begin());
+
+    timer.stop();
+
+  });
+}
+
+template <typename Key, typename Value, typename Dist>
+std::enable_if_t<(sizeof(Key) != sizeof(Value)), void> static_map_insert_or_apply_sync(
+  nvbench::state& state, nvbench::type_list<Key, Value, Dist>)
+{
+  state.skip("Key should be the same type as Value.");
+}
+
+NVBENCH_BENCH_TYPES(static_map_insert_or_apply_sync,
+                    NVBENCH_TYPE_AXES(defaults::KEY_TYPE_RANGE,
+                                      defaults::VALUE_TYPE_RANGE,
+                                      nvbench::type_list<distribution::uniform>))
+  .set_name("static_map_insert_or_apply_sync_uniform_multiplicity")
+  .set_type_axes_names({"Key", "Value", "Distribution"})
+  .set_max_noise(defaults::MAX_NOISE)
+  .add_int64_axis("Multiplicity", {1, 10, 100, 1000, 10000, 100000})
+  .add_int64_axis("NumInputs", {1, 10, 100, 1000, 10000, 100000});
